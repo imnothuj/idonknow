4529708b6245f0450c0285fe0f1e231947612647
imnothuj <127659536+imnothuj@users.noreply.github.com>
2023-03-12 11:04:49 +0100

    Add files via upload

---
+++ majnkraft.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        system("START .\\snowflakes.exe");
+    }
+    return 0;
+}
---
+++ snowflakes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+int main()
+{
+    system("mode 109,32");
+    while(TRUE)
+    {
+        printf("   █","\n");
+        printf("         █","\n");
+        printf("          █","\n");
+        printf("          █","\n");
+        printf("   █","\n");
+        printf("         █","\n");
+        printf("          █","\n");
+        printf("          █","\n");
+    }
+    return 0;
+}
